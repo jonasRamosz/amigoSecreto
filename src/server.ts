c173299f8cb8c133de54adf55da1b708947e21bb
jonas.ramos <jonas.ramos@g4flex.com.br>
2024-03-07 09:02:35 -0300

    feat: first server config

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import https from "https";
+import http from "http";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const runServer = (port: number, server: http.Server) => {
+  server.listen(port, () => {
+    console.log("🚀 Running at PORT: ", port);
+  });
+};
+
+const regularServer = http.createServer(app);
+
+if (process.env.NODE_ENV === "production") {
+  //configurar o SSL
+  //roda o server na 80 e na 443
+} else {
+  const serverPort: number = process.env.PORT
+    ? parseInt(process.env.PORT)
+    : 9000;
+  runServer(serverPort, regularServer);
+}
