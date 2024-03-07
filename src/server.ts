1ce0bbb278c2a4f7f902d33a19cc965d95ad2355
jonas.ramos <jonas.ramos@g4flex.com.br>
2024-03-07 09:43:26 -0300

    feat: ping route and interceptor add to server

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,19 @@ import "dotenv/config";
 import cors from "cors";
 import https from "https";
 import http from "http";
+import siteRoutes from "./routes/site";
+import { requestIntercepter } from "./utils/requestintercepter";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.all("*", requestIntercepter);
+
+app.use("/", siteRoutes);
+//app.use('/admin', adminRoutes)
 
 const runServer = (port: number, server: http.Server) => {
   server.listen(port, () => {
